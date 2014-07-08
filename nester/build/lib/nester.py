5f417ed84adfd17f868bb9b1a30dcded51935f75
sean <seancug@hotmail.com>
2014-07-08 18:27:22 +0800

    bulid a distribution and install it

--- nester/build/lib/nester.py
+++ nester/build/lib/nester.py
@@ -1,8 +1,8 @@
+"""print list function, all of the deatil"""
 def print_list(the_list):
+	"""the_list is the list"""
 	for each_item in the_list:
 		if isinstance(each_item,list):
 			print_list(each_item)
 		else:
 			print(each_item)
-movies=["moive","ate",["hello","deewd"]]
-print_list(movies),
---
+++ nester/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         = 'nester',
+    version      = '1.0.0',
+    py_modules   = ['nester'],
+    author       = 'sean',
+    author_email = 'seancug@hotmail.com',
+    url          = 'seancug@hotmail.com',
+    description  = 'A simple printer of lister',
+    )
